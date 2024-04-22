8f283acef2fd16710489c33834c8b005545f9028
Akhilan Ganeshkumar <AkhilanG2025@gmail.com>
2024-04-22 14:07:34 -0400

    More departure board stuff that doesnt work

--- Assets/Scripts/UI Scripts/DepartureBoard.cs
+++ Assets/Scripts/UI Scripts/DepartureBoard.cs
@@ -7,7 +7,7 @@ public class DepartureBoard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Vector2(Screen.width / -2, 0);
+        transform.SetPosition(new Vector2(Screen.width / -2, 0));
     }
 
     // Update is called once per frame

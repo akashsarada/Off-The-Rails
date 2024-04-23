2c0814e096522b8057b81adc242601b20ae8d99a
Akash Sarada <akashsarada@gmail.com>
2024-04-23 13:42:22 -0400

    Updated ProjectVersion
    Updated Unity project version to 2023.3.25f1
    Fixed errors in DepartureBoard.cs

--- Assets/Scripts/UI Scripts/DepartureBoard.cs
+++ Assets/Scripts/UI Scripts/DepartureBoard.cs
@@ -7,7 +7,6 @@ public class DepartureBoard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.SetPosition(new Vector2(Screen.width / -2, 0));
     }
 
     // Update is called once per frame
@@ -20,13 +19,13 @@ public class DepartureBoard : MonoBehaviour
 	{
 		Debug.Log(":)");
 		
-		transform.LeanMoveLocal(new Vector2(175, 200), 1).setEaseOutCubic();
+		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 + 200), 1).setEaseOutCubic();
 	}
 	
 	void OnMouseExit()
 	{
 		Debug.Log(":(");
 		
-		transform.LeanMoveLocal(new Vector2(175, -150), 1).setEaseOutCubic();
+		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 - 150), 1).setEaseOutCubic();
 	}
 }

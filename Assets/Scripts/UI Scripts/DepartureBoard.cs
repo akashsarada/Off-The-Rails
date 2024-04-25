ddd4c3cd532fb44b31c5e8b04e0eaf060b7a2106
Akhilan Ganeshkumar <AkhilanG2025@gmail.com>
2024-04-25 10:33:26 -0400

    Commit pt2

--- Assets/Scripts/UI Scripts/DepartureBoard.cs
+++ Assets/Scripts/UI Scripts/DepartureBoard.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DepartureBoard : MonoBehaviour
 {
+    public AudioSource src;
+    public AudioClip clip1, clip2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +21,23 @@ public class DepartureBoard : MonoBehaviour
 	
 	void OnMouseEnter()
 	{
+<<<<<<< HEAD
 		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 + 200), 0.5f).setEaseOutCubic();
+=======
+		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 + 200), 1).setEaseOutCubic();
+        src.clip = clip1;
+        src.Play();
+>>>>>>> 849d60138368627f709edeab8f7c68454437cbc9
 	}
 	
 	void OnMouseExit()
 	{
+<<<<<<< HEAD
 		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 - 150), 0.5f).setEaseOutCubic();
+=======
+		transform.LeanMoveLocal(new Vector2(-Screen.width / 2 + 175, -Screen.height / 2 - 150), 1).setEaseOutCubic();
+        src.clip = clip2;
+        src.Play();
+>>>>>>> 849d60138368627f709edeab8f7c68454437cbc9
 	}
 }
